3af557864516bdaa40880e4b9a92695ef35baa37
LEEDDONG <ehdgus2580@naver.com>
2023-09-16 22:59:25 +0900

    first commit

---
+++ login.js
@@ -0,0 +1,8 @@
+function check(form) {
+    if (form.email.value=="ehdgus2580@naver.com"&&form.password.value=="@izawa7806") {
+        window.open('target.html');
+    }
+    else{
+        alert('Eroor Email or Password');
+    }
+}
